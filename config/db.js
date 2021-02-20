d8328a10e1def83e35065dfacd9a506ebf59aff6
Will Andreae <wcandreae@gmail.com>
2021-02-20 16:59:55 -0500

    added mongoose connection

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 'use strict'
-
+const mongoose = require('mongoose')
 // creating a base name for the mongodb
 const mongooseBaseName = 'GA_hackathon_2021'
-console.log('db is: ', process.env.DB_URI)
+// console.log('db is: ', process.env.DB_URI)
 // const DB_URI = 'mongodb+srv://dbAdmin:compost123@nyc-compost-sites.fmdip.mongodb.net/GA_hackathon_2021?retryWrites=true&w=majority'
 
 // create the mongodb uri for development and test
@@ -19,4 +19,13 @@ const localDb = process.env.TESTENV ? database.test : database.development
 // heroku production evironment otherwise use test or development db
 const currentDb = process.env.DB_URI || localDb
 
-module.exports = currentDb
+// establish database connection
+// use new version of URL parser
+// use createIndex instead of deprecated ensureIndex
+mongoose.connect(currentDb, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+  })
+
+module.exports = mongoose
