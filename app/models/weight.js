4db510c7115401a03e86ab64e63f57e09ec71d44
Will Andreae <wcandreae@gmail.com>
2021-02-20 16:59:41 -0500

    added comments

--- app/models/weight.js
+++ app/models/weight.js
@@ -16,4 +16,20 @@ const weightSchema = new mongoose.Schema({
   timestamps: true,
 });
 
+
+// const mongoose = require('mongoose')
+// const reviewSchema = require('./review')
+
+// const profileSchema = new mongoose.Schema({
+//   city: String,
+//   state: { type: String, required: true },
+//   instrument: { type: String, required: true },
+//   interest: { type: String, required: true },
+//   blurb: String,
+//   reviews: [reviewSchema],
+//   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+// }, {
+//   timestamps: true
+// })
+
 module.exports = mongoose.model("User", weightSchema);
