ad26483932c36d448af5900a5b9cfcb4e5db4b05
Will Andreae <wcandreae@gmail.com>
2021-02-21 13:14:45 -0500

    added code to initiate mongodb upload

--- app/seed.js
+++ app/seed.js
@@ -1,10 +1,32 @@
+console.log("loading mongoose");
+require("dotenv").config();
+
+// to run this file brinbg .env into the same directory level
+
 // require our instance of mongoose we created
-const mongoose = require('../config/db')
-
+const mongoose = require("../config/db");
 // Schema
-const Locations = require('./models/location')
-
+// const Locations = mongoose.model("Location");
+const Locations = require("./models/location");
+console.log("booting up locationschemas: ", Locations);
 // importing json locations (the data)
-const { locations } = require('./locations')
+const { locations } = require("./locations");
 
-Locations.insertMany(locations).then((res) => console.log(res))
+console.log("json locations: ", locations);
+
+console.log("inserting many: ", locations);
+async function loadLocations() {
+  try {
+    await Locations.insertMany(locations);
+    console.log("Done!");
+    process.exit();
+  } catch (e) {
+    console.log(e);
+    process.exit();
+  }
+}
+
+// This is the cleanup script... run if all else fails
+// Locations.deleteMany({})
+
+// loadLocations();
