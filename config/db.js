e4488d95da617699bcbc4ee5559b7068c474298e
Will Andreae <wcandreae@gmail.com>
2021-02-21 13:16:21 -0500

    removed console logs

--- config/db.js
+++ config/db.js
@@ -1,31 +1,29 @@
-'use strict'
-const mongoose = require('mongoose')
+"use strict";
+const mongoose = require("mongoose");
 // creating a base name for the mongodb
-const mongooseBaseName = 'GA_hackathon_2021'
-// console.log('db is: ', process.env.DB_URI)
-// const DB_URI = 'mongodb+srv://dbAdmin:compost123@nyc-compost-sites.fmdip.mongodb.net/GA_hackathon_2021?retryWrites=true&w=majority'
+const mongooseBaseName = "GA_hackathon_2021";
 
 // create the mongodb uri for development and test
 const database = {
   development: `mongodb://localhost/${mongooseBaseName}-development`,
-  test: `mongodb://localhost/${mongooseBaseName}-test`
-}
+  test: `mongodb://localhost/${mongooseBaseName}-test`,
+};
 
 // Identify if development environment is test or development
 // select DB based on whether a test file was executed before `server.js`
-const localDb = process.env.TESTENV ? database.test : database.development
+const localDb = process.env.TESTENV ? database.test : database.development;
 
 // Environment variable DB_URI will be available in
 // heroku production evironment otherwise use test or development db
-const currentDb = process.env.DB_URI || localDb
+const currentDb = process.env.DB_URI || localDb;
 
 // establish database connection
 // use new version of URL parser
 // use createIndex instead of deprecated ensureIndex
 mongoose.connect(currentDb, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true
-  })
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
-module.exports = mongoose
+module.exports = mongoose;
