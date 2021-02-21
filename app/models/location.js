1caf58d671f2619fb14443b0be90813ed1793b89
Will Andreae <wcandreae@gmail.com>
2021-02-21 13:15:44 -0500

    trimmed data

--- app/models/location.js
+++ app/models/location.js
@@ -1,25 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require("../../config/db");
 
 const location = new mongoose.Schema(
   {
     borough: { type: String },
-    councildist: { type: String },
+    councildist: { type: Number },
     food_scrap_drop_off_site: { type: String },
     hours_from: { type: String },
     hours_to: { type: String },
-    latitude: { type: String },
+    latitude: { type: Number },
     location: { type: String },
     longitude: { type: String },
     ntaname: { type: String },
-    notes: { type: String },
-    objectid: { type: String },
     operation_day: { type: String },
     open_months: { type: String },
     serviced_by: { type: String },
+    borocd: { type: Number },
+    ct2010: { type: Number },
+    zip_code: { type: Number },
     website: { type: String },
-    borocd: { type: String },
-    ct2010: { type: String },
-    zip_code: { type: String },
+    notes: { type: String },
   },
   {
     timestamps: true,
